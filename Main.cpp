1b5788f31d5148088a43694d9b037d5e96ef3942
MeiTingTham <meiting0120@gmail.com>
2024-10-24 21:58:16 +0800

    Player name entry, line structure of the board

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int space[3][3]={{00, 01, 02}, {10, 11, 12}, {20, 21, 22}};
+
+    string n1;
+    string player2;
+    cout<<"Enter the name for the first player: \n";
+    getline(cin, n1);
+    cout<<"Enter the name of the second player: \n";
+    getline(cin, n2);
+
+    cout<<"    |    |    \n";
+    cout<<"____|____|____\n";
+    cout<<"    |    |    \n";
+    cout<<"    |    |    \n";
+    cout<<"____|____|____\n";
+    cout<<"    |    |    \n";
+    cout<<"    |    |    \n";
+}
